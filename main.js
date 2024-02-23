b6ba483537c9824e7f972d230574c5b5fc8a186c
yisoo song <yisoo1049@gmail.com>
2024-02-24 02:37:59 +0900

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY=`1ca59444f6184e01a70725c06a31ae22`
 
 
 const getMainNews= async()=>{
-    let url=`https://wemadenews.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}&pageSize=20`
+    let url=`https://ohmynews.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}&pageSize=20`
     const response= await fetch
     console.log("hier",response)
 }
